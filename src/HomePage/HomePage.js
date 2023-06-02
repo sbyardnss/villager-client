99d01403478b6d4f3c90e0a8b14b7bf1ce031e7b
Mary Williams <sbyardnss@gmail.com>
2023-06-02 15:25:10 -0500

    working on tournament display in homepage

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,36 +1,71 @@
 import "./HomePage.css"
 
 import { React, useContext, useState, useEffect } from "react"
-import { getAllCommunityPosts } from "../ServerManager"
+import { getAllCommunityPosts, getAllPlayers, getAllTournaments } from "../ServerManager"
 
 export const HomePage = () => {
+    const [players, setPlayers] = useState([])
     const [communityPosts, setCommunityPosts] = useState([])
+    const [tournaments, setTournaments] = useState([])
     useEffect(
         () => {
-            getAllCommunityPosts().then(data => setCommunityPosts(data))
+            // getAllCommunityPosts().then(data => setCommunityPosts(data))
+            Promise.all([getAllPlayers(), getAllCommunityPosts(), getAllTournaments()]).then(([playerData, communityPostData, tournamentData]) => {
+                setPlayers(playerData)
+                setCommunityPosts(communityPostData)
+                setTournaments(tournamentData)
+            })
         }, []
     )
 
     return <>
         <main id="homepageContainer">
-            <h2>Homepage</h2>
-            <article>
-                <h3>community posts</h3>
-                <section id="communityForumMsgs">
+            <h1>Homepage</h1>
+            <article key="communityForum">
+                <h2>community posts</h2>
+                <section id="communityForumMsgs" >
                     {
                         communityPosts.map(post => {
-                            return <>
-                                {post.poster.full_name} {post.message}
-                            </>
+                            return (
+                                <li key={post.id} className="communityPost">
+                                    <h3>
+                                        {post.poster.full_name}
+                                    </h3>
+                                    {post.message}</li>
+                            )
                         })
                     }
                 </section>
             </article>
-            <article>
-                <h4>open challenges</h4>
+            <article key="openChallenges">
+                <h2>open challenges</h2>
             </article>
-            <article>
-                <h4>active tournaments</h4>
+            <article key="activeTournaments">
+                <h2>active tournaments</h2>
+                <section id="activeTournaments">
+                    {
+                        tournaments.map(t => {
+                            return (
+                                <li key={t.id} className="activeTournamentListItem">
+                                    <h3>{t.title}</h3>
+                                    {
+                                        t.games.map(game => {
+                                            return (
+                                                <div>
+                                                    Round {game.tournament_round}
+                                                    <div>
+                                                        <li>white - {game.player_w.full_name}</li>
+                                                        <li>black - {game.player_b.full_name}</li>
+                                                    </div>
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </li>
+                            )
+                        })
+                    }
+                </section>
             </article>
         </main>
     </>

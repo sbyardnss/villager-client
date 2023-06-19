987e2fb4bea15b790839d35aa08a107fceb70b72
Mary Williams <sbyardnss@gmail.com>
2023-06-19 12:02:20 -0500

    updating homepage

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -8,7 +8,7 @@ export const HomePage = () => {
     const localVillager = localStorage.getItem("villager")
     const localVillagerObj = JSON.parse(localVillager)
     const navigate = useNavigate()
-    const { players, games, resetGames, selectedGameObj, selectedGame, setSelectedGame} = useContext(PlayContext)
+    const { players, games, resetGames, selectedGameObj, selectedGame, setSelectedGame } = useContext(PlayContext)
     const [communityPosts, setCommunityPosts] = useState([])
     const [tournaments, setTournaments] = useState([])
     const [myTournaments, setMyTournaments] = useState([])
@@ -60,7 +60,7 @@ export const HomePage = () => {
                 })
                 setMyTournaments(joinedTournaments)
             }
-        },[tournaments]
+        }, [tournaments]
     )
     useEffect(
         () => {
@@ -68,20 +68,20 @@ export const HomePage = () => {
                 const myTournamentGames = games.filter(g => {
                     return myTournaments.find(t => {
                         if (t.in_person === false) {
-                            return t.id === g.tournament 
+                            return t.id === g.tournament
                         }
                     })
                 })
                 setTournamentGames(myTournamentGames)
             }
-        },[myTournaments]
+        }, [myTournaments]
     )
     useEffect(
         () => {
             if (selectedGame !== 0) {
                 navigate("/play")
             }
-        },[selectedGame]
+        }, [selectedGame]
     )
     const resetCommunityPosts = () => {
         getAllCommunityPosts()
@@ -120,113 +120,114 @@ export const HomePage = () => {
     }
     return <>
         <main id="homepageContainer">
-            <h1>Homepage</h1>
-            <article id="communityForum">
-                <h2>community posts</h2>
-                <section id="communityForumMsgs" >
+            {/* <h1>Homepage</h1> */}
+            <div id="forumAndOpenChallenges">
+                <article id="communityForum">
+                    <section id="communityForumMsgs" >
+                        <h2>community posts</h2>
+                        {
+                            communityPosts.map(post => {
+                                const date_time = new Date(post.date_time)
+                                const date = date_time.toLocaleDateString('en-us')
+                                const time = date_time.toLocaleTimeString('en-us', { hour: 'numeric', minute: '2-digit' })
+                                if (post.poster?.id === localVillagerObj.userId) {
+                                    return (
+                                        <li key={post.id} className="communityPost">
+                                            <h3>
+                                                {post.poster?.username}
+                                            </h3>
+                                            <h5>{date} {time}</h5>
+                                            {post.message}
+                                            <button onClick={() => handleDelete(post.id)}>delete</button>
+                                        </li>
+                                    )
+                                }
+                                else {
+                                    return (
+                                        <li key={post.id} className="communityPost">
+                                            <h3>
+                                                {post.poster?.username}
+                                            </h3>
+                                            <h5>{date} {time}</h5>
+                                            {post.message}</li>
+                                    )
+                                }
+                            })
+                        }
+                    </section>
+                    <section id="communityForumInterface">
+                        <input id="communityForumInput"
+                            type="text"
+                            value={newPost.message}
+                            onChange={handleChange}
+                            onKeyDown={handlekeyDown}
+                        />
+                        <button id="communityForumSubmitBtn"
+                            onClick={() => {
+                                submitNewPostToAPI(newPost).then(() => {
+                                    const copy = { ...newPost }
+                                    copy.message = ""
+                                    updateNewPost(copy)
+                                    getAllCommunityPosts().then(data => setCommunityPosts(data))
+                                    // resetMessages()
+                                })
+                            }}
+                        >send</button>
+                    </section>
+                </article>
+                <div id="openChallengesList">
+                    <h2>open challenges</h2>
                     {
-                        communityPosts.map(post => {
-                            const date_time = new Date(post.date_time)
-                            const date = date_time.toLocaleDateString('en-us')
-                            const time = date_time.toLocaleTimeString('en-us', { hour: 'numeric', minute: '2-digit' })
-                            if (post.poster?.id === localVillagerObj.userId) {
+                        challenges?.map(c => {
+                            const challengingPlayer = c.player_w ? c.player_w : c.player_b
+                            if (challengingPlayer.id !== localVillagerObj.userId) {
                                 return (
-                                    <li key={post.id} className="communityPost">
-                                        <h3>
-                                            {post.poster?.full_name}
-                                        </h3>
-                                        <h5>{date} {time}</h5>
-                                        {post.message}
-                                        <button onClick={() => handleDelete(post.id)}>delete</button>
-                                    </li>
-                                )
-                            }
-                            else {
-                                return (
-                                    <li key={post.id} className="communityPost">
-                                        <h3>
-                                            {post.poster?.full_name}
-                                        </h3>
-                                        <h5>{date} {time}</h5>
-                                        {post.message}</li>
+                                    <div key={c.id} className="challengeListItem">
+                                        <div>
+                                            Challenger: {challengingPlayer.full_name} playing as <span id={c.player_w ? "whiteChallengeSpan" : "blackChallengeSpan"}>{c.player_w ? "white" : "black"}</span>
+                                        </div>
+                                        <div>
+                                            <button className="challengeBtn"
+                                                onClick={() => {
+                                                    const copy = { ...c }
+                                                    c.player_w ? copy.player_b = localVillagerObj.userId : copy.player_w = localVillagerObj.userId
+                                                    c.player_w ? copy.player_w = c.player_w.id : copy.player_b = c.player_b.id
+                                                    copy.accepted = true
+                                                    acceptChallenge(copy)
+                                                        .then(() => resetGames())
+                                                }}>accept</button> and play as {c.player_w ? "black" : "white"}
+                                        </div>
+                                    </div>
                                 )
                             }
                         })
                     }
-                </section>
-                <section id="communityForumInterface">
-                    <input id="communityForumInput"
-                        type="text"
-                        value={newPost.message}
-                        onChange={handleChange}
-                        onKeyDown={handlekeyDown}
-                    />
-                    <button id="communityForumSubmitBtn"
-                        onClick={() => {
-                            submitNewPostToAPI(newPost).then(() => {
-                                const copy = { ...newPost }
-                                copy.message = ""
-                                updateNewPost(copy)
-                                getAllCommunityPosts().then(data => setCommunityPosts(data))
-                                // resetMessages()
-                            })
-                        }}
-                    >send</button>
-                </section>
-            </article>
+                </div>
+            </div>
             <article key="challenges">
-                <section>
-                    <h2>Active Games</h2>
-                    <div id="myActiveGames">
-                        {
-                            myUnfinishedGames?.map(ug => {
-                                const opponent = ug.player_w?.id === localVillagerObj.userId ? ug.player_b : ug.player_w
-                                const isTournamentGame = () => {
-                                    return ug.tournament ? "tournamentChallengeListItem" : "challengeListItem"
-                                }
-                                if (opponent) {
-                                    return (
-                                        <div key={ug.id} className={isTournamentGame()}>
-                                            <div>Playing as <span id={ug.player_w.id === localVillagerObj.userId ? "whiteChallengeSpan" : "blackChallengeSpan"}>{ug.player_w?.id === localVillagerObj.userId ? "white" : "black"}</span></div>
-                                            <div>Against: {opponent.full_name}</div>
-                                            <button className="challengeBtn"
-                                                    onClick={() => {
-                                                        setSelectedGame(ug.id)
-                                                    }}>play</button>
-                                        </div>
-                                    )
-                                }
-                            })
-                        }
-                    </div>
-                    <h2>open challenges</h2>
-                    <div id="openChallengesList">
-                        {
-                            challenges?.map(c => {
-                                const challengingPlayer = c.player_w ? c.player_w : c.player_b
-                                if (challengingPlayer.id !== localVillagerObj.userId) {
-                                    return (
-                                        <div key={c.id} className="challengeListItem">
-                                            <div>
-                                                Challenger: {challengingPlayer.full_name} playing as <span id={c.player_w ? "whiteChallengeSpan" : "blackChallengeSpan"}>{c.player_w ? "white" : "black"}</span>
-                                            </div>
-                                            <div>
-                                                <button className="challengeBtn"
-                                                    onClick={() => {
-                                                        const copy = { ...c }
-                                                        c.player_w ? copy.player_b = localVillagerObj.userId : copy.player_w = localVillagerObj.userId
-                                                        c.player_w ? copy.player_w = c.player_w.id : copy.player_b = c.player_b.id
-                                                        copy.accepted = true
-                                                        acceptChallenge(copy)
-                                                            .then(() => resetGames())
-                                                    }}>accept</button> and play as {c.player_w ? "black" : "white"}
-                                            </div>
-                                        </div>
-                                    )
-                                }
-                            })
-                        }
-                    </div>
+                <section id="myActiveGames">
+                    <h2>My Games</h2>
+                    {
+                        myUnfinishedGames?.map(ug => {
+                            // const opponent = ug.player_w?.id === localVillagerObj.userId ? ug.player_b : ug.player_w
+                            const opponent = ug.player_w?.id === localVillagerObj.userId ? players.find(p => p.id === ug.player_b?.id) : players.find(p => p.id === ug.player_w?.id)
+                            const isTournamentGame = () => {
+                                return ug.tournament ? "tournamentActiveGameListItem" : "activeGameListItem"
+                            }
+                            if (opponent) {
+                                return (
+                                    <div key={ug.id} className={isTournamentGame()}>
+                                        <div>Play as <span id={ug.player_w.id === localVillagerObj.userId ? "whiteChallengeSpan" : "blackChallengeSpan"}>{ug.player_w?.id === localVillagerObj.userId ? "white" : "black"}</span></div>
+                                        <div className="opponentSectionForListItem">Opponent: {opponent.username}</div>
+                                        <button className="challengeBtn"
+                                            onClick={() => {
+                                                setSelectedGame(ug.id)
+                                            }}>play</button>
+                                    </div>
+                                )
+                            }
+                        })
+                    }
                 </section>
                 <section>
                     <h2>create challenge</h2>
@@ -245,6 +246,19 @@ export const HomePage = () => {
                                 updateChallengeForApi(challengeCopy)
                             }}>black</div>
                         </div>
+                        <div id="randomSelect" onClick={() => {
+                            const challengeCopy = { ...challengeForApi }
+                            const randomNumber = Math.floor(Math.random() * 2)
+                            if (randomNumber === 1) {
+                                challengeCopy.player_w = localVillagerObj.userId
+                                challengeCopy.player_b = null
+                            }
+                            else {
+                                challengeCopy.player_b = localVillagerObj.userId
+                                challengeCopy.player_w = null
+                            }
+                            updateChallengeForApi(challengeCopy)
+                        }}>random</div>
                         <button onClick={() => {
                             if (window.confirm("create open challenge?")) {
                                 sendNewGame(challengeForApi)
@@ -255,13 +269,13 @@ export const HomePage = () => {
             </article>
             <article>
                 <h2>my tournaments</h2>
-                        {
-                            myTournaments?.map(t => {
-                                return (
-                                    <div key={t.id}>{t.title}</div>
-                                )
-                            })
-                        }
+                {
+                    myTournaments?.map(t => {
+                        return (
+                            <div key={t.id}>{t.title}</div>
+                        )
+                    })
+                }
             </article>
         </main>
     </>

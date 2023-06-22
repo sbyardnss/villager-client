ab18b4cc794d593daaeb5c263c9b757a30f397b1
Mary Williams <sbyardnss@gmail.com>
2023-06-21 21:50:39 -0500

    navMenu response working correctly

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -27,21 +27,21 @@ export const Nav = () => {
     const [unread, setUnread] = useState(0)
     const navigate = useNavigate()
     //close open menu
-    const closeOpenMenus = (e) => {
-        if (navMenu.current && !navMenu.current.contains(e.target)) {
-            document.getElementById("active").checked = false
-        }
-        //why does adding the conditional work here??!!! this function now allows me to close the menu if and only if the user clicks outside of the menu or manually closes it with the icon
-        if (!navMenu.current && document?.getElementById("active")) {
-            if (document.getElementById("active").contains(e.target)) {
-                document.getElementById("active").checked = false
-            }
-        }
-    }
-    const closeMenuAction = () => {
-        document.getElementById("active").checked = false
-    }
-    document.addEventListener(`click`, closeOpenMenus)
+    // const closeOpenMenus = (e) => {
+    //     if (navMenu.current && !navMenu.current.contains(e.target)) {
+    //         document.getElementById("active").checked = false
+    //     }
+    //     //why does adding the conditional work here??!!! this function now allows me to close the menu if and only if the user clicks outside of the menu or manually closes it with the icon
+    //     if (!navMenu.current && document?.getElementById("active")) {
+    //         if (document.getElementById("active").contains(e.target)) {
+    //             document.getElementById("active").checked = false
+    //         }
+    //     }
+    // }
+    // const closeMenuAction = () => {
+    //     document.getElementById("active").checked = false
+    // }
+    // document.addEventListener(`click`, closeOpenMenus)
 
     // const msgNotification = () => {
     //     if (unread !== 0) {
@@ -50,7 +50,19 @@ export const Nav = () => {
     //         </>
     //     }
     // }
-    
+    const check = (e) => {
+        if (e.target.checked === true) {
+            // document.getElementById(e.target.id).checked = false
+            return false
+        }
+        else {
+            // document.getElementById(e.target.id).checked = true
+            return true
+        }
+    }
+    const closeMenuOnNavigate = () => {
+        document.getElementById("active").checked = false
+    }
     return (
         <header className="navigation" id="navMenu">
             <div id="logo">
@@ -59,21 +71,22 @@ export const Nav = () => {
                     {/* <img id="graffiti_logo" src={graffiti}></img> */}
                 </Link>
             </div>
-            <div id="navList" ref={(navMenu)}>
-                <input type="checkbox" id="active" />
+            {/* <div id="navList" ref={(navMenu)}> */}
+            <div id="navList" >
+                <input type="checkbox" id="active" onClick={()=> check}/>
                 <label htmlFor="active" className="menu-btn" ><span></span></label>
                 <label htmlFor="inactive" className="close"></label>
                 <ul id="navLinks">
                     <div>
-                        <li className="tempListItem"><Link id="/profile" className="profileLink" to="/profile" onClick={closeMenuAction}>Profile</Link></li>
-                        <li className="tempListItem"><Link id="/play" className="navigation_link" to="/play" onClick={closeMenuAction}>Play</Link></li>
+                        <li className="tempListItem"><Link id="/profile" className="profileLink" to="/profile" onClick={() => closeMenuOnNavigate()}>Profile</Link></li>
+                        <li className="tempListItem"><Link id="/play" className="navigation_link" to="/play" onClick={() => closeMenuOnNavigate()}>Play</Link></li>
                     </div>
                     <div>
-                        <li className="tempListItem"><Link id="/messages" className="navigation_link" to="/messages" onClick={closeMenuAction}>Messages </Link></li>
-                        <li className="tempListItem"><Link id="/tournament" className="navigation_link" to="/tournament" onClick={closeMenuAction}>Tournaments</Link></li>
+                        <li className="tempListItem"><Link id="/messages" className="navigation_link" to="/messages" onClick={() => closeMenuOnNavigate()}>Messages </Link></li>
+                        <li className="tempListItem"><Link id="/tournament" className="navigation_link" to="/tournament" onClick={() => closeMenuOnNavigate()}>Tournaments</Link></li>
                     </div>
                     <div>
-                        <li className="tempListItem"><Link id="/userList" className="navigation_link" to="/userList" onClick={closeMenuAction}>Make Friends</Link></li>
+                        <li className="tempListItem"><Link id="/userList" className="navigation_link" to="/userList" onClick={() => closeMenuOnNavigate()}>Make Friends</Link></li>
                         <li className="tempListItem"><Link id="logout" className="navigation_logout" to="" onClick={() => {
                             localStorage.removeItem("villager")
                             navigate("/", { replace: true })

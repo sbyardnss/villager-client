8b3b7e80371b34c31d1df23e257b815ec623c70d
Mary Williams <sbyardnss@gmail.com>
2023-06-27 10:24:41 -0500

    fixed background on challenges and styled messaging component

--- src/Messages/Messages.js
+++ src/Messages/Messages.js
@@ -61,17 +61,25 @@ export const Messages = () => {
     }
     const selectChatOrActiveChat = () => {
         if (selectedChat !== 0) {
+            const chatRecipient = friends?.find(f => f.id = selectedChat)
             return <>
                 <div id="fullChatAndInterface">
+                    {chatRecipient ?
+
+                        <div id="chatRecipientHeader" className=" setCustomFont">
+                            {chatRecipient.username}
+                            <button className="buttonStyleReject"
+                                onClick={() => setSelectedChat(0)}>exit chat</button>
+                        </div>
+                        : ""}
                     <section id="chat">
                         {
                             selectedChatMsgs.map(msg => {
                                 const date = new Date(msg.date_time).toLocaleDateString('en-us')
                                 const time = new Date(msg.date_time).toLocaleTimeString('en-us', { hour: 'numeric', minute: '2-digit' })
-                                
                                 if (msg.recipient === localVillagerObj.userId) {
                                     return (
-                                        <li key={msg.id} className="receivedMsg">
+                                        <li key={msg.id +'--' +msg.sender} className="receivedMsg">
                                             <div>{msg.message}</div>
                                             <div className="msgDateTimeFormat">{date}</div>
                                             <div className="msgDateTimeFormat">{time}</div>
@@ -80,7 +88,7 @@ export const Messages = () => {
                                 }
                                 else {
                                     return (
-                                        <li key={msg.id} className="sentMsg">
+                                        <li key={msg.id +'--' +msg.recipient} className="sentMsg">
                                             <div>{msg.message}</div>
                                             <div className="msgDateTimeFormat">{date}</div>
                                             <div className="msgDateTimeFormat">{time}</div>
@@ -90,9 +98,9 @@ export const Messages = () => {
                             })
                         }
                     </section>
-                    <div>
+                    <div id="chatInterface">
                         <input
-                            id="chatInterface"
+                            id="chatInput"
                             type="text"
                             className="text-input"
                             value={newMsg.message}
@@ -132,12 +140,12 @@ export const Messages = () => {
     return <>
         <main id="messagingContainer">
             <section id="messagesFriends">
-                {/* {selectedChat === 0 ? <div className="setCustomFont">select chat</div> : ""} */}
-                <ul id="messagingFriendList">
+                {selectedChat === 0 ? <div className="setCustomFont setSize">select chat</div> : ""}
+                <ul id={selectedChat === 0 ? "messagingFriendList" : "activeChatMessagingFriendList"}>
                     {
                         friends?.map(f => {
                             return (
-                                <li key={f.id}
+                                <li key={f.id + '--' + f.username}
                                     className={f.id === selectedChat ? "selectedFriendListItem" : "messagingFriendListItem"}
                                     onClick={() => setSelectedChat(f.id)}>
                                     {f.full_name}

46cd0cffc20e3df624ab49a8db973b1ce82d3328
Mary Williams <sbyardnss@gmail.com>
2023-06-30 16:12:20 -0500

    updated

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -9,30 +9,7 @@ export const Nav = () => {
     const navMenu = useRef(null)
     const [unread, setUnread] = useState(0)
     const navigate = useNavigate()
-    //close open menu
-    // const closeOpenMenus = (e) => {
-    //     if (navMenu.current && !navMenu.current.contains(e.target)) {
-    //         document.getElementById("active").checked = false
-    //     }
-    //     //why does adding the conditional work here??!!! this function now allows me to close the menu if and only if the user clicks outside of the menu or manually closes it with the icon
-    //     if (!navMenu.current && document?.getElementById("active")) {
-    //         if (document.getElementById("active").contains(e.target)) {
-    //             document.getElementById("active").checked = false
-    //         }
-    //     }
-    // }
-    // const closeMenuAction = () => {
-    //     document.getElementById("active").checked = false
-    // }
-    // document.addEventListener(`click`, closeOpenMenus)
 
-    // const msgNotification = () => {
-    //     if (unread !== 0) {
-    //         return <>
-    //             <div id="newMsgCount">{unread}</div>
-    //         </>
-    //     }
-    // }
     const check = (e) => {
         if (e.target.checked === true) {
             // document.getElementById(e.target.id).checked = false

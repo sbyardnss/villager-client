325c3858c3987742b17dd373b57310a444bb5765
Mary Williams <sbyardnss@gmail.com>
2023-08-15 16:21:48 -0500

    put results modal in own component

--- src/Tournament/ActiveTournament.js
+++ src/Tournament/ActiveTournament.js
@@ -6,6 +6,7 @@ import { EditPlayersModal } from "./EditPlayersModal"
 import { Swiss } from "tournament-pairings"
 import { TournamentTable } from "./TournamentTable"
 import { EditScores } from "./EditScores"
+import { ResultsModal } from "./ResultsModal"
 
 
 export const ActiveTournament = () => {
@@ -19,6 +20,8 @@ export const ActiveTournament = () => {
     const [currentRound, setCurrentRound] = useState(0)
     const [editScores, setEditScores] = useState(false)
     const [viewTable, setViewTable] = useState(false)
+    const [showResults, setShowResults] = useState(false)
+    const [showEndTournament, setShowEndTournament] = useState(false)
     // const [editPlayers, setEditPlayers] = useState(false)
 
     //tournament process state variables
@@ -96,9 +99,6 @@ export const ActiveTournament = () => {
             if (activeTournament.pairings) {
                 let opponentObj = {}
                 activeTournament.pairings.map(p => {
-                    // if (p.player1 === 'g7'){
-                    //     console.log(p.player2)
-                    // }
                     if (!opponentObj[p.player1] && p.player1) {
                         opponentObj[p.player1] = []
                     }
@@ -147,8 +147,6 @@ export const ActiveTournament = () => {
             }
         }, [activeTournament]
     )
-
-
     //getting current round pairings updating bye game if necessary
     useEffect(
         () => {
@@ -177,8 +175,6 @@ export const ActiveTournament = () => {
             if (byePairing) {
                 const byeCopy = { ...gameForApi }
                 byeCopy.player_b = null
-                // byeCopy.player_w = byePairing.player1
-                // byeCopy.winner = byePairing.player1
                 byeCopy.bye = true
                 byeCopy.win_style = ""
                 if (typeof byePairing.player1 === 'string') {
@@ -331,94 +327,6 @@ export const ActiveTournament = () => {
         })
     }
 
-    //creating solkoff tie break data
-    const solkoffTieBreaker = (playerIdArr) => {
-        const solkoffTieBreakerArr = []
-        for (const playerId of playerIdArr) {
-            let count = 0
-            const playerMatchupResults = resultsForTieBreak.filter(result => {
-                return result.white === playerId || result.black === playerId
-            })
-            for (const result of playerMatchupResults) {
-                const opponentId = playerId === result.white ? result.black : result.white
-
-                //THIS LINE BELOW WAS CAUSING A NaN ERROR WHERN REFERENCING OPPONENTSCORE
-                //REMEMBER THIS
-                // const opponentScore = document.getElementById(`${opponentId}-- score`).innerHTML 
-
-                if (opponentId !== undefined && scoreObj[opponentId]) {
-                    count += scoreObj[opponentId]
-                }
-            }
-            solkoffTieBreakerArr.push([playerId, count])
-        }
-        return solkoffTieBreakerArr
-    }
-    //creating cumulative tie break data
-    const cumulativeTieBreaker = (playerIdArr) => {
-        const cumulativeArr = []
-        for (const playerId of playerIdArr) {
-            let count = 0
-            const playerMatchupResults = resultsForTieBreak.filter(result => {
-                return result.white === playerId || result.black === playerId
-            })
-            for (const result of playerMatchupResults) {
-                const opponentId = result.white === playerId ? result.black : result.white
-                // console.log(opponentId)
-                // console.log(typeof playerId)
-                result.winner === playerId && opponentId !== undefined ? count += (count + 1)
-                    : result.win_style === 'draw' ? count += (count + .5)
-                        : count = count + count
-            }
-            cumulativeArr.push([playerId, count])
-        }
-        return cumulativeArr
-    }
-    //compiling tie break data
-    const tieBreakDisplay = (arrForTie) => {
-        const solkoffResultsArr = solkoffTieBreaker(arrForTie).sort((a, b) => { return b[1] - a[1] })
-        const cumulativeResultsArr = cumulativeTieBreaker(arrForTie).sort((a, b) => { return b[1] - a[1] })
-        return (
-            <div id="tieBreakResults">
-                <div id="fullResults">
-                    <div className="resultsHeader">solkoff</div>
-                    <div id="solkoffResults">
-                        {
-                            solkoffResultsArr.map(playerResult => {
-                                const player = typeof playerResult[0] === 'string' ? allPlayersArr.find(player => player.guest_id === playerResult[0])
-                                    : allPlayersArr.find(player => player.id === playerResult[0])
-                                return (
-                                    <div key={playerResult[0] + '--' + playerResult[1]} className="resultsModalListItem">
-                                        <div>{player?.full_name}: </div>
-                                        <div>{playerResult[1]}</div>
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-                </div>
-                <div id="fullResults">
-                    <div className="resultsHeader">cumulative</div>
-                    <div id="cumulativeResults">
-                        {
-                            cumulativeResultsArr.map(playerResult => {
-                                const player = typeof playerResult[0] === 'string' ? allPlayersArr.find(player => player.guest_id === playerResult[0])
-                                    : allPlayersArr.find(player => player.id === playerResult[0])
-                                return (
-                                    <div key={playerResult[0] + '--' + playerResult[1]} className="resultsModalListItem">
-                                        <div>{player?.full_name}: </div>
-                                        <div>{playerResult[1]}</div>
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-                </div>
-            </div>
-
-        )
-    }
-    // console.log(playerOpponentsReferenceObj)
     //update game for api either initial or updating
     const handleGameForApiUpdate = (targetId, whitePieces, blackPieces, pastGame) => {
         let copy = {}
@@ -664,63 +572,18 @@ export const ActiveTournament = () => {
         if (activeTournament && activeTournamentPlayers) {
             const endTournamentModal = document.getElementById('endTournamentModal')
             const modal = document.getElementById('resultsModal')
-            //results modal display
-            const resultsDisplay = () => {
-                const results = {}
-                const resultArr = []
-                const arrForTieBreakers = []
-                allPlayersArr.map(player => {
-                    const playerIdentifier = player.guest_id ? player.guest_id : player.id
-                    if (player.guest_id) {
-                        resultArr.push([player.full_name, parseFloat(scoreObj[playerIdentifier]), player.guest_id])
-                        arrForTieBreakers.push(player.guest_id)
-                    }
-                    else {
-                        resultArr.push([player.full_name, parseFloat(scoreObj[playerIdentifier]), player.id])
-                        arrForTieBreakers.push(player.id)
-                    }
-                })
-                resultArr.sort((a, b) => { return b[1] - a[1] })
-                return (
-                    <section id="fullResults" className="setCustomFont">
-                        <div id="standardResults" >
-                            <div className="resultsHeader">standard</div>
-                            {
-                                resultArr.map(r => {
-                                    return (
-                                        <div key={r[0]} className="resultsModalListItem">
-                                            <div>{r[0]}: </div>
-                                            <div>{r[1].toString()}</div>
-                                        </div>
-                                    )
-                                })
-                            }
-                        </div>
-                        {tieBreakDisplay(arrForTieBreakers)}
-                    </section>
-                )
-            }
             return <>
                 <main id="tournamentContainer">
-                    <div id="resultsModal">
-                        Results
-                        {resultsDisplay()}
-                        <div id="modalBtns">
-                            {activeTournament.complete === false ?
-                                <button
-                                    className="buttonStyleApprove"
-                                    onClick={() => {
-                                        endTournamentModal.style.display = "flex"
-                                        modal.style.display = "none"
-                                    }}>End Tournament</button>
-                                : ""}
-                            <button
-                                className="buttonStyleReject"
-                                onClick={() => {
-                                    modal.style.display = "none"
-                                }}>cancel</button>
-                        </div>
-                    </div>
+                    {showResults ?
+                        <ResultsModal
+                            activeTournament={activeTournament}
+                            allPlayersArr={allPlayersArr}
+                            scoreObj={scoreObj}
+                            setShowResults={setShowResults}
+                            resultsForTieBreak={resultsForTieBreak}
+                            setShowEndTournament={setShowEndTournament}
+                        />
+                        : ""}
                     <div id="endTournamentModal" className="setCustomFont">
                         End Tournament?
                         <div id="endTournamentBtnBlock">
@@ -852,7 +715,8 @@ export const ActiveTournament = () => {
                         <button
                             className="progressionControlBtn controlBtnApprove"
                             onClick={() => {
-                                modal.style.display = "flex";
+                                // modal.style.display = "flex";
+                                setShowResults(true)
                             }}>Results</button>
                     </div>
                     <div className="setColor setTournamentFontSize">
@@ -873,12 +737,12 @@ export const ActiveTournament = () => {
                             : ""}
                     </article>
                     {editScores ?
-                        <EditScores 
-                            allPlayersArr = {allPlayersArr}
-                            handleGameForApiUpdate = {handleGameForApiUpdate}
-                            setEditScores = {setEditScores}
+                        <EditScores
+                            allPlayersArr={allPlayersArr}
+                            handleGameForApiUpdate={handleGameForApiUpdate}
+                            setEditScores={setEditScores}
                             gameForApi={gameForApi}
-                            updateGameForApi = {updateGameForApi}
+                            updateGameForApi={updateGameForApi}
                         />
                         : ""}
                 </main>

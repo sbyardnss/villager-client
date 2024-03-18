22e464c54470c09df3098ac6acf2b1295b723c58
Mary Williams <sbyardnss@gmail.com>
2024-03-18 09:19:14 -0500

    need to revise play provider before continuing testing

--- cypress/e2e/tournament/specs/active-tournament.cy.js
+++ cypress/e2e/tournament/specs/active-tournament.cy.js
@@ -22,8 +22,27 @@ describe('Active Tournament Functionality', () => {
       .should('exist');
     cy.get('[data-cy="tournament--1"]')
       .click();
-    // cy.wait('@loadTournamentGames');
+    cy.wait('@loadTournamentGames');
     // cy.log('what is the issue');
     // debugger
   })
 })
+
+/*
+REQUESTS:
+
+HOMEPAGE
+get_active_user_games
+players
+get_open_challenges
+get_active_user_games
+get_open_challenges
+players
+games
+my_clubs
+get_my_clubs_posts
+
+
+chessClubs
+
+*/

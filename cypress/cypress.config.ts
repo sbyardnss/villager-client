87daa65994b34daa3327afbc10893434dd2a9ca2
Mary Williams <sbyardnss@gmail.com>
2024-03-21 11:32:57 -0500

    added video output to testing

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "cypress";
-
+//to run cypress without opening: npx cypress run --config-file cypress/cypress.config.ts
 export default defineConfig({
   e2e: {
     supportFile: false,
@@ -7,5 +7,8 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    video: true,
+    videoCompression: true, // Enables video compression
+    videosFolder: 'cypress/videos', // Specifies the folder where videos are stored
   },
 });

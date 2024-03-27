e4eab8e6dd59a1e90e8bc0454a4e3b7a03c2e51d
Mary Williams <sbyardnss@gmail.com>
2024-03-27 12:50:29 -0500

    initial test and beforeEach ready

--- cypress/integrations/specs/tournament.spec.ts
+++ cypress/integrations/specs/tournament.spec.ts
@@ -5,20 +5,17 @@
 import { setupTournaments } from '../../actions/setup-tournaments';
 import { setupHomepage } from '../../actions/setup-homepage';
 
-describe('Selecting a tournament from list loads tournament', () => {
+describe('Tournament Components', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000');
     setupHomepage();
     setupTournaments();
-  });
-  it('should exist', () => {
     cy.login();
     cy.visit('/tournament');
-    // cy.wait('@loadMyTournaments');
-    // cy.wait('@loadClubMates');
-    // cy.wait('@loadChessClubs');
+  });
+  it('When user selects a tournament, the tournament and its games should be loaded', () => {
     cy.get('[data-cy="select-tournament--153"]')
       .click();
-    cy.wait('@loadTournamentGames').its('response.body').its('length').should('be.gt', 0);
+    cy.wait('@loadTournamentGames').its('response.body').its('length').should('equal', 6);
   });
 })
